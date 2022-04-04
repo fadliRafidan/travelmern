c317881e37c3a754981b27bb0cc18f41599688c5
fadliRafidan <rafidfid28@gmail.com>
2022-04-05 01:16:33 +0700

    Create Header component and put to LandingPage

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,16 @@
+import Header from 'parts/Header';
+import React, { Component } from 'react';
+
+class LandingPage extends Component {
+    render() {
+        return (
+            <>
+                <Header {...this.props}>
+
+                </Header>
+            </>
+        );
+    }
+}
+
+export default LandingPage;
